a9018f1b59950da57700fba6b27b47338bee3ec9
Chetanya-joshi <108217245+Chetanya-joshi@users.noreply.github.com>
2023-06-08 15:37:25 +0530

    Update mice.js

--- src/components/mice.js
+++ src/components/mice.js
@@ -5,9 +5,10 @@ import { Container, Row, Col, Modal, Button} from 'react-bootstrap';
 import { Link} from 'react-router-dom';
 import offer1 from './imagegallery/socialmedia.png'
 import offer2 from './imagegallery/socialmediaad.png'
-import offer3 from './imagegallery/seo.png'
+// import offer3 from './imagegallery/seo.png'
 const Mice = () => {
   const [showModal, setShowModal] = useState(false);
+  const [showModal2, setShowModal2] = useState(false);
 
   const handleModalClose = () => {
     setShowModal(false);
@@ -16,27 +17,32 @@ const Mice = () => {
   const handleImageClick = () => {
     setShowModal(true);
   };
+
+
+  const handleModalClose2 = () => {
+    setShowModal2(false);
+  };
+
+  const handleImageClick2 = () => {
+    setShowModal2(true);
+  };
   return (
     <div>
       <Container>
-        <h1 className="mt-5 text-white text-center">Digital Marketing</h1>
+        <h1 className="mt-5 text-white text-center">MICE</h1>
         <h3 className="mt-3  text-white text-center">What We Offer</h3>
-        <Row className="mt-4" style={{pointerEvents:'cursor'}}>
-          <Col md={4}>
+        <Row className="mt-4" style={{pointerEvents:'cursor', display:'flex' , justifyContent:'center'}}>
+          <Col md={4} style={{cursor:'pointer'}}>
             <div className="image-container" onClick={handleImageClick}>
               <img src={offer1} alt="Image 1" className="img-fluid" />
             </div>
           </Col>
-          <Col md={4}>
-            <div className="image-container" onClick={handleImageClick}>
+          <Col md={4} style={{cursor:'pointer'}}>
+            <div className="image-container" onClick={handleImageClick2}>
               <img src={offer2} alt="Image 2" className="img-fluid" />
             </div>
           </Col>
-          <Col md={4}>
-            <div className="image-container" onClick={handleImageClick}>
-              <img src={offer3} alt="Image 3" className="img-fluid" />
-            </div>
-          </Col>
+          
         </Row>
 
         <Modal show={showModal} onHide={handleModalClose} dialogClassName="custom-modal w-1000" >
@@ -44,21 +50,29 @@ const Mice = () => {
             <Modal.Title>What We Offer</Modal.Title>
           </Modal.Header>
           <Modal.Body>
-            <h4>Social Media Marketing:</h4>
-            <h9>It is an essential tool for businesses to connect with their target audience and build brand awareness. It allows companies to reach a wide range of potential customers with tailored messages and engaging content, resulting in increased conversions.</h9>
-            <h4>Social Media Advertising:</h4>
-            <h9>media advertising also offers detailed analytics and reporting, enabling businesses to
-              track the performance of their ads in real-time. This data-driven approach allows for
-              continuous optimization, ensuring that ad campaigns are cost-effective and delivering
-              the desired results.	</h9>
-            <h4>SEO</h4>
-            <h9>It is a crucial digital marketing strategy aimed at
-              improving a website's visibility and organic search rankings on search engine result
-              pages. By optimizing various aspects of a website, such as content, keywords, meta
-              tags, and backlinks, SEO helps businesses attract more targeted organic traffic.</h9>
+            <h4>Business Events:</h4>
+            <h9>Business events are a valuable platform for businesses to connect, engage, and expand their networks. Whether it's conferences, trade shows, or networking events, business events offer numerous benefits. They provide opportunities for businesses to showcase their products or services, gain exposure, and attract potential customers. Business events foster face-to-face interactions, enabling businesses to build valuable relationships with industry professionals, partners, and clients. These events also offer a platform for knowledge sharing, learning about industry trends, and gaining insights from experts. Moreover, business events can lead to collaborations, partnerships, and new business opportunities, ultimately driving growth and success.</h9>
           </Modal.Body>
           <Modal.Footer>
             <Button variant="secondary" onClick={handleModalClose}>
+              Close
+            </Button>
+          </Modal.Footer>
+        </Modal>
+
+
+
+        <Modal show={showModal2} onHide={handleModalClose2} dialogClassName="custom-modal w-1000" >
+          <Modal.Header closeButton>
+            <Modal.Title>What We Offer</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <h4>Annual Business Meetings:</h4>
+            <h9>Annual business meetings are essential gatherings that bring together key stakeholders, executives, and employees to discuss the progress, goals, and strategies of the organization. These meetings offer several benefits. They provide a platform for aligning the team, sharing important updates, and fostering a sense of unity and shared purpose. Annual business meetings facilitate collaboration and idea-sharing, allowing for innovative solutions and improved decision-making. They also offer an opportunity to recognize and celebrate achievements, boosting morale and motivation within the organization. Moreover, annual business meetings help set the direction for the coming year, ensuring everyone is on the same page and committed to driving the organization's success.</h9>
+            
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleModalClose2}>
               Close
             </Button>
           </Modal.Footer>
