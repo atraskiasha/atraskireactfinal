5ddbff65d7d490f3a91fc75f4dd3f4ae063ccd9a
atraski asha <atraskiashasharma@gmail.com>
2023-06-13 16:23:52 +0530

    main

--- src/components/Main.js
+++ src/components/Main.js
@@ -6,10 +6,8 @@
 /* eslint-disable jsx-a11y/img-redundant-alt */
 import React, { useState, useEffect } from 'react';
 import { Container, Alert } from 'react-bootstrap';
-// import { FaSearch } from 'react-icons/fa';
-
 // import Button from 'react-bootstrap/Button';
-
+import '../index.css';
 
 import logo1 from './images/bm.jpeg';
 import logo2 from './images/ducati.jpeg';
@@ -25,43 +23,63 @@ import handImage from './images/hand.png'; // Import your hand image
 
 // import logo10 from './images/atraski logo new -2.png';
 const sentences = [
-    'Want to join Digital Marketing team?',
-    'Need to build Public Relation in market?',
-    'Want to create an Event with us?',
-    'Are you looking for a web developer?',
+  'Want to join Digital Marketing team?',
+   'Need to build Public Relation in market?',
+    'Want to create an Event with us?', 
+    'Are you looking for a web developer?', 
     'Do you want to create Influencer Marketing?',
-    'Want to work on the mice solution ?',
-    // 'Join us in revolution and pave your path to business success.',
-    // 'Discover the limitless opportunities marketing and unlock your business.',
-    // 'Take charge of your entrepreneurial journey by mastering digital marketing',
+     'Want to work on the mice solution ?',
   ];
 
 const Main = () => {
   
   
   // const SentenceBox = () => {
+    const [isHovered, setIsHovered] = useState(false);
+
     const [currentSentenceIndex, setCurrentSentenceIndex] = useState(0);
     const [isHandTapping, setIsHandTapping] = useState(false);
   
-    useEffect(() => {
-      const interval = setInterval(() => {
-        setCurrentSentenceIndex((prevIndex) => (prevIndex + 1) % sentences.length);
-      }, 3000); // Change sentence every 3 seconds
+
+    const handleMouseEnter = () => {
+      setIsHovered(true);
+    };
+    
+    const handleMouseLeave = () => {
+      setIsHovered(false);
+    };
+
+    // useEffect(() => {
+    //   const interval = setInterval(() => {
+    //     setCurrentSentenceIndex((prevIndex) => (prevIndex + 1) % sentences.length);
+    //   }, 3000); // Change sentence every 3 seconds
   
-      return () => clearInterval(interval);
-    }, []);
+    //   return () => clearInterval(interval);
+    // }, []);
   
     useEffect(() => {
       const handTapTimeout = setTimeout(() => {
         setIsHandTapping(true);
         const tapEndTimeout = setTimeout(() => {
           setIsHandTapping(false);
-        }, 500); 
+        }, 500); // Tap animation duration
         return () => clearTimeout(tapEndTimeout);
-      }, 1000); 
+      }, 1000); // Delay before starting tap animation
       return () => clearTimeout(handTapTimeout);
     }, [currentSentenceIndex]);
   
+    useEffect(() => {
+      const intervals = setInterval(() => {
+        if (!isHovered) {
+          setCurrentSentenceIndex((prevIndex) => (prevIndex + 1) % sentences.length);
+        }
+      }, 3000);
+  
+      return () => clearInterval(intervals);
+    }, [isHovered]);
+  
+    
+
     const currentSentence = sentences[currentSentenceIndex];
 
   return (
@@ -73,24 +91,40 @@ const Main = () => {
 
       <div className="background">
         <div className="content">
-          <h1 style={{ fontSize: '40px' }}>Welcome to Atraski</h1>
-          <h3 id="mainheading" style={{ marginTop: '-15px', fontSize: '20px' }}>Hello, World!
+          <h1>Welcome to Atraski</h1>
+          <h3 id="mainheading" style={{ marginTop: '-15px', fontSize: '20px' }}>
+            <span className="desktop-only">Hello, World!
             We believe in inspiring India in a unique and innovative way. Every day,
             we offer a wide range of services across events, travel, blogging, and fashion to bring you the best of what we
-            have to offer.We promise to inspire and delight you every step of the way.Atraski flourishing furiously over the past years is now proud to present a cabinet of successful events throughout all A tier cities of the country including open mics, fashion walks and walks promoting social causes, bloggers meets, art confluences along with travel meets and packaged tours all across India</h3>
+            have to offer.We promise to inspire and delight you every step of the way.Atraski flourishing furiously over the past years is now proud to present a cabinet of successful events throughout all A tier cities of the country including open mics, fashion walks and walks promoting social causes, bloggers meets, art confluences along with travel meets and packaged tours all across India</span>
+            <span className="mobile-only">
+          Hello, World!
+            We believe in inspiring India in a unique and innovative way. Every day,
+            we offer a wide range of services across events, travel, blogging, and fashion to bring you the best of what we
+            have to offer.
+            </span>
+            </h3>
 
+                   
+            
           <a href="/chat" className="containerchat">
           <Container className="d-flex align-items-center justify-content-center">
+          {/* <input type="search" style={{width:'5%' , marginBottom:'15px'}} /> */}
+          
       <Alert
+        id="changeText"
         variant="primary"
         className="text-center position-relative"
-        style={{ width: `${currentSentence.length}ch`, fontSize: '1.5rem', backgroundColor: '#FFFAF4' }}
+        style={{ width: '65ch', fontSize: '1.3rem', backgroundColor: '#F9F9F9'}}
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
       >
-        {isHandTapping && <img src={handImage} alt="Hand" className="hand-image" />}
+        
+        {isHandTapping && <img src={handImage} alt="Hand" className="hand-image"/>}
         {currentSentence}
       </Alert>
+      
     </Container>
-    
        
           </a>
 
