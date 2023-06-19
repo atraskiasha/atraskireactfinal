4a77ad8fd1d9275bc1ed7b5c499a7d9983821e16
atraski asha <atraskiashasharma@gmail.com>
2023-06-19 12:22:58 +0530

    main page

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,14 +1,9 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable jsx-a11y/heading-has-content */
-/* eslint-disable jsx-a11y/anchor-has-content */
-/* eslint-disable jsx-a11y/anchor-is-valid */
-/* eslint-disable react-hooks/exhaustive-deps */
-/* eslint-disable jsx-a11y/img-redundant-alt */
-import React, { useState, useEffect } from 'react';
-import { Container, Alert} from 'react-bootstrap';
+// import React, { useState, useEffect } from 'react';
+// import { Container, Alert } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
-// import Button from 'react-bootstrap/Button';
+import { useState} from 'react';
 import '../index.css';
+
 
 import logo1 from './images/bm.jpeg';
 import logo2 from './images/ducati.jpeg';
@@ -19,117 +14,131 @@ import logo6 from './images/cme.jpeg';
 import logo7 from './images/eazydiner-prime.png';
 import logo8 from './images/mcmv.jpeg';
 import logo9 from './images/travel.jpeg';
-import handImage from './images/hand.png'; // Import your hand image
- 
+// import handImage from './images/hand.png'; // Import your hand image
+// import searchIcon from './images/search.png'; // Import your search icon
 
-// import logo10 from './images/atraski logo new -2.png';
-const sentences = [
-  'Want to join Digital Marketing team?',
-   'Need to build Public Relation in market?',
-    'Want to create an Event with us?', 
-    'Are you looking for a web developer?', 
-    'Do you want to create Influencer Marketing?',
-     'Want to work on the mice solution ?',
-  ];
+// const sentences = [
+//   'Want to join Digital Marketing team?',
+//   'Need to build Public Relation in market?',
+//   'Want to create an Event with us?',
+//   'Are you looking for a web developer?',
+//   'Do you want to create Influencer Marketing?',
+//   'Want to work on the mice solution ?',
+// ];
 
 const Main = () => {
-  
-  
-  // const SentenceBox = () => {
-    const [isHovered, setIsHovered] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedOption, setSelectedOption] = useState('');
+  const [searchTerm] = useState('');
 
-    const [currentSentenceIndex, setCurrentSentenceIndex] = useState(0);
-    const [isHandTapping, setIsHandTapping] = useState(false);
-  
+  const options = ['Want to join Digital Marketing team?', 'Need to build Public Relation in market?', 'Want to create an Event with us?'];
 
-    const handleMouseEnter = () => {
-      setIsHovered(true);
-    };
-    
-    const handleMouseLeave = () => {
-      setIsHovered(false);
-    };
+  const handleToggle = () => {
+    setIsOpen(!isOpen);
+  };
 
-    // useEffect(() => {
-    //   const interval = setInterval(() => {
-    //     setCurrentSentenceIndex((prevIndex) => (prevIndex + 1) % sentences.length);
-    //   }, 3000); // Change sentence every 3 seconds
-  
-    //   return () => clearInterval(interval);
-    // }, []);
-  
-    useEffect(() => {
-      const handTapTimeout = setTimeout(() => {
-        setIsHandTapping(true);
-        const tapEndTimeout = setTimeout(() => {
-          setIsHandTapping(false);
-        }, 500); // Tap animation duration
-        return () => clearTimeout(tapEndTimeout);
-      }, 1000); // Delay before starting tap animation
-      return () => clearTimeout(handTapTimeout);
-    }, [currentSentenceIndex]);
-  
-    useEffect(() => {
-      const intervals = setInterval(() => {
-        if (!isHovered) {
-          setCurrentSentenceIndex((prevIndex) => (prevIndex + 1) % sentences.length);
-        }
-      }, 3000);
-  
-      return () => clearInterval(intervals);
-    }, [isHovered]);
-  
-    
+  const handleOptionSelect = (option) => {
+    setSelectedOption(option);
+    setIsOpen(false);
+  };
 
-    const currentSentence = sentences[currentSentenceIndex];
+  // const handleSearch = (event) => {
+  //   setSearchTerm(event.target.value);
+  // };
+
+  const filteredOptions = options.filter((option) =>
+    option.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+  
+ 
+  // const [isHovered, setIsHovered] = useState(false);
+  // const [currentSentenceIndex, setCurrentSentenceIndex] = useState(0);
+  // // const [isHandTapping, setIsHandTapping] = useState(false);
+
+  // const handleMouseEnter = () => {
+  //   setIsHovered(true);
+  // };
+
+  // const handleMouseLeave = () => {
+  //   setIsHovered(false);
+  // };
+
+  // useEffect(() => {
+  //   const handTapTimeout = setTimeout(() => {
+  //     // setIsHandTapping(true);
+  //     const tapEndTimeout = setTimeout(() => {
+  //       // setIsHandTapping(false);
+  //     }, 500); // Tap animation duration
+  //     return () => clearTimeout(tapEndTimeout);
+  //   }, 1000); // Delay before starting tap animation
+  //   return () => clearTimeout(handTapTimeout);
+  // }, [currentSentenceIndex]);
+
+  // useEffect(() => {
+  //   const intervals = setInterval(() => {
+  //     if (!isHovered) {
+  //       setCurrentSentenceIndex((prevIndex) => (prevIndex + 1) % sentences.length);
+  //     }
+  //   }, 3000);
+
+  //   return () => clearInterval(intervals);
+  // }, [isHovered]);
+
+  // const currentSentence = sentences[currentSentenceIndex];
 
   return (
     <>
-      {/* "homepage": "https://github.com/atraskiasha/atraskireact.git", */}
-
-     
-
-
       <div className="background">
         <div className="content">
           <h1>Welcome to Atraski</h1>
           <h3 id="mainheading" style={{ marginTop: '-15px', fontSize: '20px' }}>
-            <span className="desktop-only">Hello, World!
-            We believe in inspiring India in a unique and innovative way. Every day,
-            we offer a wide range of services across events, travel, blogging, and fashion to bring you the best of what we
-            have to offer.We promise to inspire and delight you every step of the way.Atraski flourishing furiously over the past years is now proud to present a cabinet of successful events throughout all A tier cities of the country including open mics, fashion walks and walks promoting social causes, bloggers meets, art confluences along with travel meets and packaged tours all across India</span>
-            <span className="mobile-only">
-          Hello, World!
-            We believe in inspiring India in a unique and innovative way. Every day,
-            we offer a wide range of services across events, travel, blogging, and fashion to bring you the best of what we
-            have to offer.
-            </span>
-            </h3>
+            <span className="desktop-only">Hello, World! We believe in inspiring India in a unique and innovative way. Every day, we offer a wide range of services across events, travel, blogging, and fashion to bring you the best of what we have to offer. We promise to inspire and delight you every step of the way. Atraski flourishing furiously over the past years is now proud to present a cabinet of successful events throughout all A tier cities of the country including open mics, fashion walks and walks promoting social causes, bloggers meets, art confluences along with travel meets and packaged tours all across India</span>
+            <span className="mobile-only">Hello, World! We believe in inspiring India in a unique and innovative way. Every day, we offer a wide range of services across events, travel, blogging, and fashion to bring you the best of what we have to offer.</span>
+          </h3>
+          {/* <a href="/chat" className="containerchat"> */}
+          {/* <Container className="d-flex align-items-center justify-content-center">
+              <div style={{ position: 'relative', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                <img src={searchIcon} alt="Search" style={{ width: '20px', height: '20px' }} />
+                <Alert
+                  id="changeText"
+                  variant="primary"
+                  className="text-center position-relative"
+                  style={{ width: '65ch', fontSize: '1.3rem', backgroundColor: '#F9F9F9' }}
+                  onMouseEnter={handleMouseEnter}
+                  onMouseLeave={handleMouseLeave}
+                > */}
+          {/* {isHandTapping && <img src={handImage} alt="Hand" className="hand-image" />} */}
+          {/* {currentSentence} */}
+          {/* </Alert> */}
+          {/* </div> */}
+          {/* </Container> */}
+          {/* </a> */}
+          <div onClick={handleToggle}             style={{ cursor:'pointer' }}
+>
+        <input
+          type="text"
+          value={selectedOption}
+          placeholder="Select an option"
+          readOnly
+          // style={{ width: selectedOption.length * 8, color: 'blue' }}
+          style={{ width: selectedOption ? '70vh' : '350px',height:'5vh',borderRadius:'7px', color: 'black', backgroundColor:'',textAlign:'center',fontSize:'1.1rem' }}
 
-                   
-            
-          <a href="/chat" className="containerchat">
-          <Container className="d-flex align-items-center justify-content-center">
-          {/* <input type="search" style={{width:'5%' , marginBottom:'15px'}} /> */}
+          />
+        <span>&#x25BC;</span>
+      </div>
+      {isOpen && (
+        <div>
           
-      <Alert
-        id="changeText"
-        variant="primary"
-        className="text-center position-relative"
-        style={{ width: '65ch', fontSize: '1.3rem', backgroundColor: '#F9F9F9'}}
-        onMouseEnter={handleMouseEnter}
-        onMouseLeave={handleMouseLeave}
-      >
+          {filteredOptions.map((option) => (
+            <a href='/chat'><div key={option} onClick={() => handleOptionSelect(option)}
+            style={{ cursor: 'pointer', fontSize: '1rem', textAlign: 'center', margin: '-1.3rem 0' }}
+            >
+              {option}
+            </div></a>
+          ))}
+        </div>
+      )}
         
-        {isHandTapping && <img src={handImage} alt="Hand" className="hand-image"/>}
-        {currentSentence}
-      </Alert>
-      
-    </Container>
-       
-          </a>
-
-
         </div>
       </div>
 
@@ -147,7 +156,7 @@ const Main = () => {
           backgroundAttachment: 'fixed',
           height: '100vh',
           position: 'relative',
-          zIndex:'-1'
+          zIndex: '-1'
 
         }}
       >
@@ -263,11 +272,11 @@ const Main = () => {
       </div>
 
 
-      <footer className=" text-white" style={{backgroundColor:'#333'}}>
+      <footer className=" text-white" style={{ backgroundColor: '#333' }}>
         <div className="container">
           <div className="row">
             <div className="col-md-6">
-            <p><i className="fa fa-envelope"></i> Email: abhyuday.majhi@atraski.com</p>
+              <p><i className="fa fa-envelope"></i> Email: abhyuday.majhi@atraski.com</p>
               <p><i className="fa fa-envelope"></i>Email: priti.jhawar@atraski.com</p>              <p><i className="fa fa-phone"></i> Contact No: 9163720198</p>
               <p><i className="fa fa-phone"></i> Contact No: 7596009785</p>
             </div>
