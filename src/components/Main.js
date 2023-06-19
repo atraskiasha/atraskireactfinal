083b119129e668e1374af14770983e3c1f9330bb
atraski asha <atraskiashasharma@gmail.com>
2023-06-19 17:46:49 +0530

    main

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,7 @@
 // import React, { useState, useEffect } from 'react';
-// import { Container, Alert } from 'react-bootstrap';
+import { Container, Alert } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
-import React, { useState, useEffect ,useMemo} from 'react';
+import React, { useState,useEffect } from 'react';
 import '../index.css';
 
 // <<<<<<< HEAD
@@ -31,87 +31,52 @@ import logo9 from './images/travel.png';
  
 // >>>>>>> 2744392e7ec3026a8b8da4f0aa9489d6f4c11f85
 
-// const sentences = [
-//   'Want to join Digital Marketing team?',
-//   'Need to build Public Relation in market?',
-//   'Want to create an Event with us?',
-//   'Are you looking for a web developer?',
-//   'Do you want to create Influencer Marketing?',
-//   'Want to work on the mice solution ?',
-// ];
+const sentences = [
+  'Want to join Digital Marketing team?',
+  'Need to build Public Relation in market?',
+  'Want to create an Event with us?',
+  'Are you looking for a web developer?',
+  'Do you want to create Influencer Marketing?',
+  'Want to work on the mice solution ?',
+];
 
 const Main = () => {
-  const [isOpen, setIsOpen] = useState(false);
-  const [selectedOption, setSelectedOption] = useState('Want to join Digital Marketing team?');
-  const [searchTerm] = useState('');
-
-  const options = useMemo(() => ['Want to join Digital Marketing team?', 'Need to build Public Relation in market?', 'Want to create an Event with us?'], []);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      const currentIndex = options.indexOf(selectedOption);
-      const nextIndex = (currentIndex + 1) % options.length;
-      setSelectedOption(options[nextIndex]);
-    }, 2000);
-
-    return () => {
-      clearInterval(interval);
-    };
-  }, [selectedOption, options]);
-
-  const handleToggle = () => {
-    setIsOpen(!isOpen);
-  };
-
-  const handleOptionSelect = (option) => {
-    setSelectedOption(option);
-    setIsOpen(false);
-  };
-
-  // const handleSearch = (event) => {
-  //   setSearchTerm(event.target.value);
-  // };
-  
-
-  const filteredOptions = options.filter((option) =>
-    option.toLowerCase().includes(searchTerm.toLowerCase())
-  );
   
  
-  // const [isHovered, setIsHovered] = useState(false);
-  // const [currentSentenceIndex, setCurrentSentenceIndex] = useState(0);
-  // // const [isHandTapping, setIsHandTapping] = useState(false);
+  const [isHovered, setIsHovered] = useState(false);
+  const [currentSentenceIndex, setCurrentSentenceIndex] = useState(0);
+  // const [isHandTapping, setIsHandTapping] = useState(false);
 
-  // const handleMouseEnter = () => {
-  //   setIsHovered(true);
-  // };
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  };
 
-  // const handleMouseLeave = () => {
-  //   setIsHovered(false);
-  // };
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
 
-  // useEffect(() => {
-  //   const handTapTimeout = setTimeout(() => {
-  //     // setIsHandTapping(true);
-  //     const tapEndTimeout = setTimeout(() => {
-  //       // setIsHandTapping(false);
-  //     }, 500); // Tap animation duration
-  //     return () => clearTimeout(tapEndTimeout);
-  //   }, 1000); // Delay before starting tap animation
-  //   return () => clearTimeout(handTapTimeout);
-  // }, [currentSentenceIndex]);
+  useEffect(() => {
+    const handTapTimeout = setTimeout(() => {
+      // setIsHandTapping(true);
+      const tapEndTimeout = setTimeout(() => {
+        // setIsHandTapping(false);
+      }, 500); // Tap animation duration
+      return () => clearTimeout(tapEndTimeout);
+    }, 1000); // Delay before starting tap animation
+    return () => clearTimeout(handTapTimeout);
+  }, [currentSentenceIndex]);
 
-  // useEffect(() => {
-  //   const intervals = setInterval(() => {
-  //     if (!isHovered) {
-  //       setCurrentSentenceIndex((prevIndex) => (prevIndex + 1) % sentences.length);
-  //     }
-  //   }, 3000);
+  useEffect(() => {
+    const intervals = setInterval(() => {
+      if (!isHovered) {
+        setCurrentSentenceIndex((prevIndex) => (prevIndex + 1) % sentences.length);
+      }
+    }, 3000);
 
-  //   return () => clearInterval(intervals);
-  // }, [isHovered]);
+    return () => clearInterval(intervals);
+  }, [isHovered]);
 
-  // const currentSentence = sentences[currentSentenceIndex];
+  const currentSentence = sentences[currentSentenceIndex];
 
   return (
     <>
@@ -122,10 +87,10 @@ const Main = () => {
             <span className="desktop-only">Hello, World! We believe in inspiring India in a unique and innovative way. Every day, we offer a wide range of services across events, travel, blogging, and fashion to bring you the best of what we have to offer. We promise to inspire and delight you every step of the way. Atraski flourishing furiously over the past years is now proud to present a cabinet of successful events throughout all A tier cities of the country including open mics, fashion walks and walks promoting social causes, bloggers meets, art confluences along with travel meets and packaged tours all across India</span>
             <span className="mobile-only">Hello, World! We believe in inspiring India in a unique and innovative way. Every day, we offer a wide range of services across events, travel, blogging, and fashion to bring you the best of what we have to offer.</span>
           </h3>
-          {/* <a href="/chat" className="containerchat"> */}
-          {/* <Container className="d-flex align-items-center justify-content-center">
+          <a href="/chat" className="containerchat">
+          <Container className="d-flex align-items-center justify-content-center">
               <div style={{ position: 'relative', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-                <img src={searchIcon} alt="Search" style={{ width: '20px', height: '20px' }} />
+                {/* <img src={searchIcon} alt="Search" style={{ width: '20px', height: '20px' }} /> */}
                 <Alert
                   id="changeText"
                   variant="primary"
@@ -133,44 +98,14 @@ const Main = () => {
                   style={{ width: '65ch', fontSize: '1.3rem', backgroundColor: '#F9F9F9' }}
                   onMouseEnter={handleMouseEnter}
                   onMouseLeave={handleMouseLeave}
-                > */}
+                >
           {/* {isHandTapping && <img src={handImage} alt="Hand" className="hand-image" />} */}
-          {/* {currentSentence} */}
-          {/* </Alert> */}
-          {/* </div> */}
-          {/* </Container> */}
-          {/* </a> */}
-          <div onClick={handleToggle}             style={{ cursor:'pointer' }}
->
-
-
-        <input
-          type="text"
-          value={selectedOption}
-          placeholder={selectedOption}
-          readOnly
-          // style={{ width: selectedOption.length * 8, color: 'blue' }}
-          style={{ width: selectedOption ? '70vh' : '350px',height:'6vh',borderRadius:'7px', color: 'black', backgroundColor:'',textAlign:'center',fontSize:'1.1rem' }}
-
-          />
-         <span className="search-icon" style={{ position: 'relative', right: '30rem', fontSize:'1.2rem' }}>
-    <i className="fas fa-search" style={{ color: 'black' }}></i>
-  </span>
-        <span>&#x25BC;</span>
-      </div>
-      {isOpen && (
-        <div>
+          {currentSentence}
+          </Alert>
+          </div>
+          </Container>
+          </a>
           
-          {filteredOptions.map((option) => (
-            <a href='/chat'><div key={option} onClick={() => handleOptionSelect(option)}
-            style={{ cursor: 'pointer', fontSize: '1rem', textAlign: 'center', margin: '-1.5rem 0' }}
-            >
-              {option}
-            </div></a>
-          ))}
-        </div>
-      )}
-       
         </div>
       </div>
 
