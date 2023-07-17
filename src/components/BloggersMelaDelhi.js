e6667a78b54212829d906d9541a74f9210e7b45a
atraski <131516113+atraskiasha@users.noreply.github.com>
2023-07-17 17:55:10 +0530

    Update BloggersMelaDelhi.js

--- src/components/BloggersMelaDelhi.js
+++ src/components/BloggersMelaDelhi.js
@@ -28,7 +28,15 @@ import pannel9 from './images/Panlist10.jpg';
 import pannel10 from './images/Panlist11.jpg';
 import pannel11 from './images/Panlist12.jpg';
 
-
+import brand1 from './images/AMRI.png';
+import brand2 from './images/bigb.png';
+import brand3 from './images/chai-break-logo.jpg';
+import brand4 from './images/dalmia.png';
+import brand5 from './images/Elleven-Logo_.png';
+import brand6 from './images/feedingIndia.png';
+import brand7 from './images/frespreso.jpg';
+import brand8 from './images/gdgpng.png';
+import brand9 from './images/hero.png';
 
 
 
@@ -41,82 +49,82 @@ import pannel11 from './images/Panlist12.jpg';
 function BloggerMelaDelhi (){
  
 
-    return (
-      <>
-        <div className="container-fluid blog-delhi">
-          <div className='blog-heading'> <h1 className='heading-1'>Bloggers Mela Delhi</h1></div>
-          <div className='blog-image'>
-            <div className='blog-image-2'></div>
-            
-          </div>
+  return (
+    <>
+      <div className="container-fluid blog-delhi">
+        <div className='blog-heading'> <h1 className='heading-1'>Bloggers Mela Delhi</h1></div>
+        <div className='blog-image'>
+          <div className='blog-image-2'></div>
           
-          <div className='ques'>
-            <div className='conatiner-fluid ques1 my-4'>
-              <h1 className='p-3 para'>What is Bloggers Mela Delhi</h1>
-              <p className='p-3 para'> Blogger’s Mela is all about paving the way for newcomers who have started their own journey as bloggers.  
+        </div>
+        {/* <div className='blog-heading'><h1 className='heading-2'>events in delhi </h1></div> */}
+        <div className='ques'>
+          <div className='conatiner-fluid ques1 my-4'>
+            <h1 className='p-3'>What is Bloggers Mela Delhi</h1>
+            <p className='p-3'> Blogger’s Mela is all about paving the way for newcomers who have started their own journey as bloggers.  
 Blogger’s Mela brings together visionaries, the thoughts of the panelists and established bloggers to explore the journey of a blogger, discuss emerging trends, and share insights on future innovation. The event is offering a platform for all the newcomers to connect, learn and be inspired.
 </p>
-            </div>
-            <div className='conatiner-fluid ques1  my-4'>
-            <h1 className='p-3 para'>What will be happening</h1>
-              <p className='p-3 para'> in which we are giving opportunity to ambassador and panellist to have good interaction with brands, also bloggers who have just started there journey will be able to have a clear vision after this event we will have fashion show, Fun activities, pop up stalls and panel discussion
-              .We have Nitin Gupta as fashion influencer and Mehak & Rituraj as travel blogger’s and Khushboo as food blogger and Fatehh singh as digital creator and Paritosh Anand as creative director, entrepreneur and public speaker 
+          </div>
+          <div className='conatiner-fluid ques1  my-4'>
+          <h1 className='p-3'>What will be happening</h1>
+            <p className='p-3'> in which we are giving opportunity to ambassador and panellist to have good interaction with brands, also bloggers who have just started there journey will be able to have a clear vision after this event we will have fashion show, Fun activities, pop up stalls and panel discussion
+            .We have Nitin Gupta as fashion influencer and Mehak & Rituraj as travel blogger’s and Khushboo as food blogger and Fatehh singh as digital creator and Paritosh Anand as creative director, entrepreneur and public speaker 
 </p>
-            </div>
-            <div className='conatiner-fluid ques1  my-4'>
-            <h1 className='p-3 para'>Why should Register</h1>
-              <p className='p-3 para'>   The event is an opportunity aimed at aspiring bloggers to conduct an insightful session with well-known brands, panelists and successful bloggers. The event is offering a platform for all the newcomers to connect, learn and be inspired.
+          </div>
+          <div className='conatiner-fluid ques1  my-4'>
+          <h1 className='p-3'>Why should Register</h1>
+            <p className='p-3'>   The event is an opportunity aimed at aspiring bloggers to conduct an insightful session with well-known brands, panelists and successful bloggers. The event is offering a platform for all the newcomers to connect, learn and be inspired.
 This year's event is going to be graced by renowned bloggers such as Nitin Gupta as a fashion influencer, Mehek and Rituraj as Travel Blogger, Khushboo as a food Blogger , Fatehh Singh as a digital creator, Paritosh Anand as a creative director, an entrepreneur, a public speaker and many more.
- 
+
 </p>
-            </div>
-            </div>
-            <div className='container-fluid big-box'>
-            <div className='container-fluid small-box'>
+          </div>
+          </div>
+          <div className='container-fluid big-box'>
+          <div className='container-fluid small-box'>
 
-            <div className='ambasador mt-4'><h1 className='h1'>Ambassadors</h1></div>
-            <div class="logos">
-    <div class="logo-slider">
-     
-            <img src={Image3} className='' alt=" "/>
-        
-  
-
-            <img src={Ambassador01} className='' alt=" "/>
-            <img src={Ambassador02} className='' alt=" "/>
-            <img src={Ambassador04} className= '' alt=" "/>
-
-            <img src={Ambassador06} className='' alt=" "/>
-            <img src={Ambassador07} className='' alt=" "/>
-            <img src={Ambassador08} className='' alt=" "/>
+          <div className='ambasador mt-4'><h1 className='h1'>Ambassadors</h1></div>
+          <div class="logos">
+  <div class="logo-slider">
+   
+          <img src={Image3} className='' alt=" "/>
+      
 
 
+          <img src={Ambassador01} className='' alt=" "/>
+          <img src={Ambassador02} className='' alt=" "/>
+          <img src={Ambassador04} className= '' alt=" "/>
 
-
-
-
-    </div>
-    <div class="logo-slider">
-
-
-
-     
- 
-
-    <img src={Ambassador09} className='' alt=" "/>
-
-    <img src={Ambassador11} className='' alt=" "/>
-    <img src={Ambassador07} className='' alt=" "/>
-                <img src={Ambassador06} className='' alt=" "/>
-                <img src={Ambassador04} className= '' alt=" "/>
-                <img src={Ambassador06} className='' alt=" "/>
+          <img src={Ambassador06} className='' alt=" "/>
+          <img src={Ambassador07} className='' alt=" "/>
+          <img src={Ambassador08} className='' alt=" "/>
 
 
 
 
 
 
-     
+  </div>
+  <div class="logo-slider">
+
+
+
+   
+
+
+  <img src={Ambassador09} className='' alt=" "/>
+
+  <img src={Ambassador11} className='' alt=" "/>
+  <img src={Ambassador07} className='' alt=" "/>
+              <img src={Ambassador06} className='' alt=" "/>
+              <img src={Ambassador04} className= '' alt=" "/>
+              <img src={Ambassador06} className='' alt=" "/>
+
+
+
+
+
+
+   
 
 
 </div>
@@ -126,54 +134,291 @@ This year's event is going to be graced by renowned bloggers such as Nitin Gupta
 </div>
 </div>
 <div className='container-fluid big-box'>
-  <div className='container-fluid small-box'>
+<div className='container-fluid small-box'>
 <div className='container-fluid ambasador mt-4'><h1 className='h1'>Panelist</h1></div>
 
 <div class="logos">
-    <div class="logo-slider1">
-     
-            <img src={pannel1} className='' alt=" "/>
-        
-  
-
-            <img src={pannel2} className='' alt=" "/>
-            <img src={pannel3} className='' alt=" "/>
-
-            <img src={pannel5} className='new-img'alt=" "/>
-
-            <img src={pannel6} className= ' 'alt=" "/>
-            <img src={pannel2} className=''alt=" "/>
-
-    </div>
-    <div class="logo-slider1">
-     
-    <img src={pannel7}className='' alt=""/>
- 
-
-    <img src={pannel8}className='' alt=" "/>
-
-    <img src={pannel9} className=''alt=" "/>
-    <img src={pannel10} className=''alt=" "/>
-    <img src={pannel11} className=''alt=" "/>
-    <img src={pannel2} className=''alt=" "/>
-
+  <div class="logo-slider1">
    
+          <img src={pannel1} className='' alt=" "/>
+      
+
+
+          <img src={pannel2} className='' alt=" "/>
+          <img src={pannel3} className='' alt=" "/>
+
+          <img src={pannel5} className=''alt=" "/>
+
+          <img src={pannel6} className= ' 'alt=" "/>
+          <img src={pannel2} className=''alt=" "/>
+
+  </div>
+  <div class="logo-slider1">
+   
+  <img src={pannel7}className='' alt=""/>
+
+
+  <img src={pannel8}className='' alt=" "/>
+
+  <img src={pannel9} className=''alt=" "/>
+  <img src={pannel10} className=''alt=" "/>
+  <img src={pannel11} className=''alt=" "/>
+  <img src={pannel2} className=''alt=" "/>
+
+ 
 </div>
 </div></div>
 
 
 </div>
-    </div>
+  </div>
 
-   
-        <div className='container-fluid ques2'>
-          <Link to="https://atraski.com/events/bloggers-mela-delhi/" target='_blank' style={{ textDecoration: "none" }}>
-            <h1 className='heading-31'>Register For Bloggers Mela Delhi</h1>
-          </Link>
-        </div>
-      </>
-    );
-  
+  <div className='brands'>
+  <div className='container-fluid brands-class'><h1 className='h2'>Brands We Have Worked With</h1></div>
+
+  <div className="slider2">
+<div className="slide-track2">
+  {/* <div className="slide2" style={{ marginLeft: "120px" }}>
+    <img
+      src="https://thumbs.dreamstime.com/b/tęczy-miłości-serca-tło-60045149.jpg"
+      style={{
+        padding: "2% 10% 2% 5%",
+        width: "100%",
+        margin: "auto",
+        height: "auto",
+      }}
+      height="100"
+      width="250"
+      alt=""
+    />
+  </div> */}
+  <div className="slide2">
+    <img
+      src="https://thumbs.dreamstime.com/b/tęczy-miłości-serca-tło-60045149.jpg"
+      style={{
+        padding: "2% 10% 2% 5%",
+        width: "350px",
+        height: "70px",
+      }}
+      height="100"
+      width="300"
+      alt=""
+    />
+  </div>
+  {/* <div className="slide2">
+    <img
+      src="https://thumbs.dreamstime.com/b/tęczy-miłości-serca-tło-60045149.jpg"
+      style={{
+        padding: "2% 10% 2% 5%",
+        width: "350px",
+        // margin: "auto",
+        height: "50px",
+      }}
+      height="100"
+      width="300"
+      alt=""
+    />
+  </div> */}
+  <div className="slide2">
+    <img
+      src={brand1}
+      style={{
+        padding: "2% 10% 2% 5%",
+        width: "350px",
+        height: "70px",
+      }}
+      height="100"
+      width="300"
+      alt=""
+    />
+  </div>
+  <div className="slide2">
+    <img
+      src={brand2}
+      className="customzoom2"
+      style={{
+        padding: "2% 10% 2% 5%",
+        width: "350px",
+        height: "70px",
+      }}
+      height="100"
+      width="300"
+      alt=""
+    />
+  </div>
+  <div className="slide2" style={{ width: "150px" }}>
+    <img
+      src={brand3}
+      style={{
+        padding: "2% 10% 2% 5%",
+
+        width: "350px",
+        height: "70px",
+      }}
+      height="100"
+      width="300"
+      alt=""
+    />
+  </div>
+  <div className="slide2">
+    <img
+      src={brand4}
+      style={{
+        padding: "2% 10% 2% 5%",
+        width: "350px",
+        height: "70px",
+      }}
+      height="100"
+      width="300"
+      alt=""
+    />
+  </div>
+  <div className="slide2" style={{ width: "250px" }}>
+    <img
+      src={brand5}
+      style={{
+        padding: "2% 10% 2% 5%",
+
+        width: "400px",
+        height: "70px",
+      }}
+      height="100"
+      width="300"
+      alt=""
+    />
+  </div>
+  <div className="slide2">
+    <img
+      src={brand6}
+      style={{
+        padding: "2% 10% 2% 5%",
+        width: "350px",
+        height: "70px",
+      }}
+      height="100"
+      width="300"
+      alt=""
+    />
+  </div>
+  <div className="slide2">
+    <img
+      src={brand7}
+      style={{
+        padding: "2% 10% 2% 5%",
+        width: "350px",
+        height: "70px",
+      }}
+      height="100"
+      width="300"
+      alt=""
+    />
+  </div>
+  <div className="slide2">
+    <img
+      src={brand8}
+      style={{
+        padding: "2% 10% 2% 5%",
+        width: "350px",
+        height: "70px",
+      }}
+      height="100"
+      width="300"
+      alt=""
+    />
+  </div>
+  <div className="slide2">
+    <img
+      src={brand9}
+      style={{
+        padding: "2% 10% 2% 5%",
+        width: "350px",
+        height: "70px",
+      }}
+      height="100"
+      width="300"
+      alt=""
+    />
+  </div>
+  <div className="slide2">
+    <img
+      src={brand1}
+      style={{
+        padding: "2% 5% 2% 5%",
+        width: "350px",
+        height: "70px",
+      }}
+      height="100"
+      width="300"
+      alt=""
+    />
+  </div>
+  <div className="slide2">
+    <img
+      src={brand2}
+      style={{
+        padding: "2% 10% 2% 5%",
+        width: "350px",
+        height: "70px",
+      }}
+      height="100"
+      width="300"
+      alt=""
+    />
+  </div>
+  <div className="slide2">
+    <img
+      src={brand3}
+      style={{
+        padding: "2% 10% 2% 5%",
+        width: "350px",
+        height: "70px",
+      }}
+      height="100"
+      width="300"
+      alt=""
+    />
+  </div>
+  <div className="slide2">
+    <img
+      src={brand4}
+      style={{
+        padding: "2% 10% 2% 5%",
+        width: "350px",
+        height: "70px",
+      }}
+      height="100"
+      width="300"
+      alt=""
+    />
+  </div>
+  <div className="slide2">
+    <img
+      src={brand5}
+      style={{
+        padding: "2% 10% 2% 5%",
+        width: "350px",
+        height: "70px",
+      }}
+      height="100"
+      width="300"
+      alt=""
+    />
+  </div>
+</div>
+</div>
+</div>
+
+
+
+ 
+      <div className='container-fluid ques2'>
+        <Link to="https://atraski.com/events/bloggers-mela-delhi/" target='_blank' style={{ textDecoration: "none" }}>
+          <h1 className='heading-3'>Register For Bloggers Mela Delhi</h1>
+        </Link>
+      </div>
+    </>
+  );
+
 }
 
 export default BloggerMelaDelhi;
