64b7c1d6bb4fae74cdb59b2f37e8ac1e1453623f
atraski <131516113+atraskiasha@users.noreply.github.com>
2023-07-17 11:29:51 +0530

    Update Test.js

--- src/components/Test.js
+++ src/components/Test.js
@@ -19,7 +19,7 @@ AOS.init();
 export default function Test() {
   return (
     <div className="container my-5 py-5">
-      <h1 className="team-heading" style={{ color: "white" }}>
+      <h1 className="heading-2 text-center" style={{ color: "white", fontWeight:'500' }}>
         {" "}
         Our Models
       </h1>
@@ -186,7 +186,56 @@ export default function Test() {
             </div>
           </div>
         </div>
-        
+
+        <div className="scrollable-card">
+          <div className="card-header">
+            <div className="team-card text-center">
+              <img className="team-img" src={model1} alt="member1" />
+              {/* <div className="team-role">CEO, CTO</div> */}
+              <div className="team-name my-4">Akanksha Wankhade </div>
+              {/* <div className="team-contacts">
+                <a href="https://www.faceboook.com" target="_blank">
+                  <BsFacebook className="team-icons" />
+                </a>
+                <a
+                  href="https://www.linkedin.com/in/shashank-singh-088426204"
+                  target="_blank"
+                >
+                  <BsLinkedin className="team-icons" />
+                </a>
+                <a href="https://github.com" target="_blank">
+                  <BsGithub className="team-icons" />
+                </a>
+              </div> */}
+            </div>
+          </div>
+        </div>
+
+
+        <div className="scrollable-card">
+          <div className="card-header">
+            <div className="team-card text-center">
+              <img className="team-img" src={model2} alt="member1" />
+              {/* <div className="team-role">CEO, CTO</div> */}
+              <div className="team-name my-4">Aman Ashok </div>
+              {/* <div className="team-contacts">
+                <a href="https://www.faceboook.com" target="_blank">
+                  <BsFacebook className="team-icons" />
+                </a>
+                <a
+                  href="https://www.linkedin.com/in/shashank-singh-088426204"
+                  target="_blank"
+                >
+                  <BsLinkedin className="team-icons" />
+                </a>
+                <a href="https://github.com" target="_blank">
+                  <BsGithub className="team-icons" />
+                </a>
+              </div> */}
+            </div>
+          </div>
+        </div>
+
       </div>
     </div>
   );
